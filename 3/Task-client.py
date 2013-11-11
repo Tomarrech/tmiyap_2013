1af3f2f5cc5300ea6f5857d2df7fc7c3809b6a28
Sergey <tomarech@gmail.com>
2013-11-11 20:06:18 +0400

    laba 3 start

--- 3/Task-client.py
+++ 3/Task-client.py
@@ -1,1 +1,14 @@
 __author__ = 'issahar'
+import socket
+
+conn = socket.socket()
+conn.connect( ("127.0.0.1", 14900))
+
+conn.send(b"Hello! \n")
+data = b""
+tmp = conn.recv(1024)
+while tmp:
+    data += tmp
+    tmp = conn.recv(1024)
+print(data.decode("utf-8"))
+conn.close()
