db73da3482c95eed3f7a64777c0fbb2b9cd24b9d
Sergey <tomarech@gmail.com>
2013-12-15 14:12:22 +0400

    5th semester project. Scan of tcp-udp ports. + make scan in threads

--- kurs/main.py
+++ kurs/main.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf_8 -*-
+import time
+import thread
+import sys
 __author__ = 'issahar'
 import socket
+from threading import *
 
+host0 = '127.0.0.1'
 host1 = '192.168.1.1'
 host2 = '192.168.1.3'
 host3 = '192.168.1.5'
 host4 = '192.168.1.7'
-hosts = [host1, host2, host3, host4]
-ports = [21, 23, 53, 67, 69, 80, 443, 445]
+host5 = '192.168.1.8'
+load_hosts = [host0, host1, host2, host3, host4, host5]
+load_ports = [21, 23, 53, 67, 69, 80, 443, 445]
 open_tcp_ports = {}
 open_udp_ports = {}
 
+# opens = {'host1': [1, 2, 3, 4, 5], 'host2': [231, 23, 543, 521]}
+
 
 def scan(hosts, ports):
-    #ports = []
-    #for x in xrange(6000):
-    #    ports.append(x)
+
+    #print 'get: ', hosts
     global open_tcp_ports
-    open_tcp_ports = {}
     global open_udp_ports
-    open_udp_ports = {}
 
     for host in hosts:
-
+        #print host
         for port in ports:
             try:
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,20 +43,40 @@ def scan(hosts, ports):
                     recv, svr = sock.recvfrom(255)
                     sock.shutdown(2)
                     sock.close()
-                    print "port %s /udp open" % port
-                    open_udp_ports.append(port)
+                    print "host:port %s:%s /udp open" % (host, port)
+                    if not host in open_udp_ports:
+                        open_udp_ports[host] = []
+                    open_udp_ports[host].append(port)
+                    print 'udp ', open_udp_ports
                 except:
+                    #print "smth was wrong..."
                     #print "port %s /udp closed" % port
-                    continue
+                    pass
 
                 #print "port %s /tcp closed"
             else:
-                print "port %s /tcp open" % port
-                open_tcp_ports.append(port)
+                #print "host:port %s:%s /tcp open" % (host, port)
+                if not host in open_tcp_ports:
+                    open_tcp_ports[host] = []
+                open_tcp_ports[host].append(port)
+                print 'tcp ', open_tcp_ports, open_udp_ports
+    #thread.exit()
 
-        print "on host %s open ports:" % host
-        print "tcp: %s; udp: %s" % (open_tcp_ports, open_udp_ports)
+def loading_data(t_hosts, t_ports, threads=1):
+    real_threads = threads if threads >= len(t_hosts) else len(t_hosts)
+    print "real thr: ", real_threads
+    hosts_on_thread = len(t_ports) / real_threads
+    print "t_hosts/thr: ", hosts_on_thread
 
+    for thr in range(real_threads):
+        start = thr * hosts_on_thread
+        stop = (thr + 1) * hosts_on_thread if thr != real_threads - 1 else real_threads
+        #print "start, stop: ", start, stop
+        #print "host for thread: ", t_hosts[start:stop]
+        worker = Thread(target=scan, args=(t_hosts[start:stop], t_ports))
+        worker.daemon = True
+        worker.start()
+        time.sleep(1)
 
-
-scan(hosts, ports)
+loading_data(load_hosts, load_ports, 2)
+#print "total:\n\ttcp: %s; udp: %s" % (open_tcp_ports, open_udp_ports)
